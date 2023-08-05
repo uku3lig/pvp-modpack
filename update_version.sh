b446ab0f7c7c6718592e1893c4252f6d00292d39
uku <uku3lig@gmail.com>
2023-08-05 19:40:06 +0200

    add version update script

---
+++ update_version.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PACK_FILE="pack.toml"
+TO_EDIT=(
+    "pack.toml"
+    "config/isxander-main-menu-credits.json"
+    "config/craftpresence.json"
+)
+
+# check if tomlq is installed, exit otherwise
+if ! command -v tomlq &> /dev/null
+then
+    echo "tomlq could not be found"
+    exit
+fi
+
+# get new version from arguments
+if [ -z "$1" ]
+then
+    echo "No version argument supplied"
+    exit
+fi
+NEW_VERSION=$1
+
+# query pack file for version with tomlq
+OLD_VERSION=$(tomlq ".version" $PACK_FILE | tr -d '"' | sed -n 's/\(.*\)+.*$/\1/p')
+echo "$OLD_VERSION -> $NEW_VERSION"
+
+# replace old version with new in all files
+for file in "${TO_EDIT[@]}"
+do
+    sed -i "s/$OLD_VERSION/$NEW_VERSION/g" "$file"
+done
+
